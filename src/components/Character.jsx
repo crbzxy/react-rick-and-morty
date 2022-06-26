7c59ddab43d7afe3816002c92d5f01ba09350b3b
crbzxy <carlos.boor@gmail.com>
2022-06-25 23:20:36 -0500

    condition style status

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -5,11 +5,11 @@ function Character({ character }) {
     <div className='text-left mb-5' >
       <h2>{character.name}</h2>
       <img className='img-fluid rounded' src={character.image} alt={character.name} />
-      <p>Status: {character.status}</p>
+   {character.status === 'Alive' ? <p className='status'>Alive</p> : <p className='dead'>Dead</p>}
       <p>Specie: {character.species}</p>
       <p>Type: {character.type}</p>
       <p>Origin: {character.origin.name}</p>
-      <p>Name: {character.location.name}</p>
+      <p>Location: {character.location.name}</p>
     </div>
   )
 }
